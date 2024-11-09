3261ce239af94afb7d1eb40b86217003a38f16e9
realtanx <hamniba8@gmail.com>
2024-11-09 14:00:40 +0800

    Create project

---
+++ HIITFitnessApp/HIITFitnessApp/HIITFitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitnessAppApp.swift
+//  HIITFitnessApp
+//
+//  Created by realtanx on 2024/11/9.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
