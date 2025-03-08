afede2c45addade304e0dc6a88bd72da27bdd0d1
realtanx <hamniba8@gmail.com>
2025-03-08 18:09:37 +0800

    Config as a menu bar app

--- Pomodoro/Pomodoro/AppDelegate.swift
+++ Pomodoro/Pomodoro/AppDelegate.swift
@@ -7,16 +7,25 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    var statusItem: NSStatusItem?
+    @IBOutlet weak var statusMenu: NSMenu!
     
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        statusItem?.menu = statusMenu
+        
+        statusItem?.button?.title = "Pomodoro"
+        statusItem?.button?.imagePosition = .imageLeading
+        statusItem?.button?.image = NSImage(systemSymbolName: "timer", accessibilityDescription: "Pomodoro")
+        statusItem?.button?.font = NSFont.monospacedDigitSystemFont(ofSize: NSFont.systemFontSize, weight: .regular)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
