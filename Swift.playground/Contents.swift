1c29babe6e9de2f9b12e860055df2387acb5b009
realtanx <hamniba8@gmail.com>
2025-04-02 21:54:14 +0800

    Add playground

---
+++ Swift.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var counter = 0
+counter += 1
+//counter ++  // Error: No ++ in Swift
+
+// type inference
+let a = 3.0
+let b = Double(3)
+let c = 3 as Double
+
+let char: Character = "a"
+let str = "bc"
+
