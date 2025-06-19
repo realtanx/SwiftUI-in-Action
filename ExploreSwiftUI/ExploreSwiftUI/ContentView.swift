3b365c9b52f51a1b54420dd3afd72d087026409d
realtanx <hamniba8@gmail.com>
2025-06-19 22:59:17 +0800

    Replace default view

--- ExploreSwiftUI/ExploreSwiftUI/ContentView.swift
+++ ExploreSwiftUI/ExploreSwiftUI/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScreenHeader(title: "Hello, world!", subtitle: "Subtitle", desc: "Normal text with two lines - Happy to explore the SwiftUI framework.")
+            Text("Yet another text view")
         }
         .padding()
     }
@@ -21,4 +19,29 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ScreenHeader: View {
+    var title: String
+    var subtitle: String
+    var desc: String
+    
+    init(title: String, subtitle: String, desc: String) {
+        self.title = title
+        self.subtitle = subtitle
+        self.desc = desc
+    }
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.blue)
+            Text(subtitle)
+                .font(.headline)
+                .fontWeight(.bold)
+            Text(desc)
+        }
+    }
 }
